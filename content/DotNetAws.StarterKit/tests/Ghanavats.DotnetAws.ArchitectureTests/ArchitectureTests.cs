573f671cac7a3f21fc1a57ee25229c30e27f1023
Saeed Ghanavat <dev_projects@ghanavats.co.uk>
2026-08-18 21:11:56 +0100

    - Fixed the Architecture Tests

--- content/DotNetAws.StarterKit/tests/Ghanavats.DotnetAws.ArchitectureTests/ArchitectureTests.cs
+++ content/DotNetAws.StarterKit/tests/Ghanavats.DotnetAws.ArchitectureTests/ArchitectureTests.cs
@@ -1,5 +1,6 @@
 using ArchUnitNET.Domain;
 using ArchUnitNET.xUnitV3;
+using Microsoft.Extensions.DependencyInjection;
 using static ArchUnitNET.Fluent.ArchRuleDefinition;
 
 namespace Ghanavats.DotnetAws.ArchitectureTests;
@@ -10,17 +11,18 @@ public class ArchitectureTests : ArchitectureTestsBase
         .That()
         .ResideInAssembly(DomainAssembly)
         .As("Domain Layer");
-    
+
     private readonly IObjectProvider<IType> PresentationLayer = Types()
         .That()
         .ResideInAssembly(PresentationAssembly)
+        .And().DoNotDependOnAny(typeof(IServiceCollection))
         .As("Presentation Layer");
 
     private readonly IObjectProvider<IType> InfrastructureLayer = Types()
         .That()
         .ResideInAssembly(InfrastructureAssembly)
         .As("Infrastructure Layer");
-    
+
     private readonly IObjectProvider<IType> ApplicationLayer = Types()
         .That()
         .ResideInAssembly(ApplicationAssembly)
@@ -30,19 +32,22 @@ public class ArchitectureTests : ArchitectureTestsBase
         .That()
         .ResideInAssembly(SharedAssembly)
         .As("Shared Project");
-    
+
     private readonly IObjectProvider<IType> ApiEndpoints = Classes()
-        .That().AreSealed().And().AreAbstract()
-        .And().ArePublic().And().ResideInAssembly(PresentationAssembly)
+        .That()
+        .ResideInAssembly(PresentationAssembly)
+        .And().ArePublic()
+        .And().AreSealed()
+        .And().AreAbstract()
         .And().HaveNameEndingWith("Endpoint")
         .As("API Endpoints");
-    
+
     private readonly IObjectProvider<Interface> RepositoryInterfaces = Interfaces()
         .That().ResideInAssembly(ApplicationAssembly)
         .And().ArePublic()
         .And().HaveNameEndingWith("Repository")
         .As("Infrastructure Repositories");
-    
+
     [Fact]
     public void Types_In_Presentation_ShouldNotDirectlyDependOnRepositories()
     {
@@ -51,7 +56,7 @@ public class ArchitectureTests : ArchitectureTestsBase
             .Because("Presentation layer should not depend on Infrastructure layer")
             .Check(Architecture);
     }
-        
+
     [Fact]
     public void Endpoints_ShouldNotDependOnRepositories()
     {
@@ -60,23 +65,22 @@ public class ArchitectureTests : ArchitectureTestsBase
             .Because("API endpoints should not directly depend on Repository interfaces")
             .Check(Architecture);
     }
-    
+
     [Fact]
     public void RepositoryInterfaces_ShouldBeImplementedInInfrastructureLayer()
     {
         Classes().That().Are(InfrastructureLayer)
-            .And().ResideInNamespace("Ghanavats.DotnetAws.Infrastructure.Repositories")
-            .And().AreInternal().And().AreSealed()
+            .And().ResideInNamespace("Ghanavats.DotnetAws.__INFRA_TO_FEATURE_NAMESPACE__.Repositories")
             .Should().ImplementAnyInterfaces(RepositoryInterfaces)
             .Because("Repository interfaces should be implemented in the Infrastructure layer")
             .Check(Architecture);
     }
-    
+
     [Fact]
     public void RepositoryInterfaces_ShouldNotBeImplementedInAnyLayerButInfrastructure()
     {
         Classes().That().AreNot(InfrastructureLayer)
-            .And().DoNotResideInNamespace("Ghanavats.DotnetAws.Infrastructure.Repositories")
+            .And().DoNotResideInNamespace("Ghanavats.DotnetAws.__INFRA_TO_FEATURE_NAMESPACE__.Repositories")
             .Should().NotImplementAnyInterfaces(RepositoryInterfaces)
             .Because("Repository interfaces should only be implemented in the Infrastructure layer")
             .Check(Architecture);
